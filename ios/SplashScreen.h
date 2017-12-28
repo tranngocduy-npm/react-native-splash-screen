d2c4fdd1caa6d98d2775e4571dc6053f64e9dd83
tranngocduy-npm <33091031+tranngocduy-npm@users.noreply.github.com>
2017-12-28 15:24:42 +0700

    Update SplashScreen.h

--- ios/SplashScreen.h
+++ ios/SplashScreen.h
@@ -6,7 +6,7 @@
  * GitHub:https://github.com/crazycodeboy
  * Email:crazycodeboy@gmail.com
  */
-#import <React/RCTBridgeModule.h>
+#import "RCTBridgeModule.h"
 
 @interface SplashScreen : NSObject<RCTBridgeModule>
 + (void)show;
